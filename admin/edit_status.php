31e14195b41245ef7b17e2221ebb6aaf64405f39
praditarif <praditaarif9@gmail.com>
2025-05-14 15:21:51 +0700

    benerin status pesanan

--- admin/edit_status.php
+++ admin/edit_status.php
@@ -2,20 +2,29 @@
 include '../koneksi.php';
 
 if ($_POST) {
-  // Mendapatkan data yang dikirim melalui POST
-  $pesananId = $_POST['pesanan_id'];
-  $statusId = $_POST['status_id'];
+    // Mendapatkan data yang dikirim melalui POST
+    $pesananId = $_POST['pesanan_id'];
+    $statusId = $_POST['status_id'];
 
-  // Memperbarui status pesanan di database
-  $query = "UPDATE pesanan SET status_id = $statusId WHERE id = $pesananId";
-  $result = pg_query($conn, $query);
+    // Verifikasi nilai status_id ada di tabel status
+    $checkStatusQuery = "SELECT * FROM status WHERE id = $statusId";
+    $checkStatusResult = pg_query($conn, $checkStatusQuery);
 
-  if ($result) {
-    echo "Status pesanan berhasil diperbarui.";
-    header('LOcation:pesanan2.php');
-  } else {
-    echo "Gagal memperbarui status pesanan.";
-  }
+    if (pg_num_rows($checkStatusResult) == 0) {
+        echo "Status ID tidak valid.";
+        exit;
+    }
+
+    // Memperbarui status pesanan di database
+    $query = "UPDATE pesanan SET status_id = $statusId WHERE id = $pesananId";
+    $result = pg_query($conn, $query);
+
+    if ($result) {
+        echo "Status pesanan berhasil diperbarui.";
+        header('Location: pesanan2.php');
+        exit;
+    } else {
+        echo "Gagal memperbarui status pesanan. Error: " . pg_last_error($conn);
+    }
 }
 ?>
-`
